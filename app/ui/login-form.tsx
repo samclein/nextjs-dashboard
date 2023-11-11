e958e033225bdbd14e84435c18f165c89b180a51
malsamax <malsamax@protonmail.com>
2023-11-11 17:43:20 +0000

    completed tut

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import {
   AtSymbolIcon,
   ExclamationCircleIcon,
   KeyIcon,
 } from "@heroicons/react/24/outline";
+import { useFormState, useFormStatus } from "react-dom";
 
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
+import { authenticate } from "@/app/lib/actions";
 import { lato } from "@/app/ui/fonts";
 
 export default function LoginForm() {
+  const [code, action] = useFormState(authenticate, undefined);
   return (
-    <form className="space-y-3">
+    <form action={action} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lato.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -58,7 +63,14 @@ export default function LoginForm() {
         </div>
         <LoginButton />
         <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+          {code === "CredentialSignin" && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p aria-live="polite" className="text-sm text-red-500">
+                Invalid credentials
+              </p>
+            </>
+          )}
         </div>
       </div>
     </form>
@@ -66,8 +78,9 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
   return (
-    <Button className="mt-4 w-full">
+    <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
