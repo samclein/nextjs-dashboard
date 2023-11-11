2afaa2f932d716ca45b3389ae888cf6f548f9795
malsamax <malsamax@protonmail.com>
2023-11-11 16:57:27 +0000

    tutorial progress

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,12 @@
 import {
   BanknotesIcon,
   ClockIcon,
-  UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+import { fetchCardData } from "@/app/lib/data";
+import { lato } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +16,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -37,7 +45,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
@@ -48,7 +56,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
+        className={`${lato.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Lato } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
+export const lato = Lato({ weight: "400", subsets: ["latin"] });
